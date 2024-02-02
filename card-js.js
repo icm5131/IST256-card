887da9c1fffc4a668c9a4cd15047ede745eff32c
Ian Melius <icm5131@psu.edu>
2024-02-01 20:06:44 -0500

    main fixed the conflict

--- card-js.js
+++ card-js.js
@@ -3,19 +3,41 @@ document.querySelector('.duplicate').addEventListener('click', function (event) 
     //No More than 10 cards
     console.log(document.querySelectorAll('.base').length);
     if (document.querySelectorAll('.base').length < 10) {
-        const newCard = document.querySelector('#cardlist .base').cloneNode(true);
-        document.querySelector('#cardlist').appendChild(newCard);
+        const newCard = document.querySelector('.cardlist .base').cloneNode(true);
+        document.querySelector('.cardlist').appendChild(newCard);
     }
 });
 
 //Change Title
 document.querySelector('#changetitle').addEventListener('click', function (e) {
-    document.querySelector('.card-title').innerHTML = "Worst Meme";
+    //document.querySelector('.card-title').innerHTML = "Worst Meme";
+    var cardtitles = document.getElementsByClassName('card-title');
+    if (cardtitles[0].innerHTML !== 'THE WORST MEME') {
+        for (var i = 0; i < cardtitles.length; i++) {
+            cardtitles[i].innerHTML = "THE WORST MEME";
+        }
+    }
+    else {
+        for (var i = 0; i < cardtitles.length; i++) {
+            cardtitles[i].innerHTML = "THE BEST MEME";
+        }
+    }
 });
 
 //change image
 document.querySelector('#changeimage').addEventListener('click', function (e) {
-    document.querySelector('.img').src = "https://github.com/elmsln.png";
+    //document.querySelector('.img').src = "https://github.com/elmsln.png";
+    var cardimgs = document.getElementsByClassName('img');
+    if (cardimgs[0].src !== "https://github.com/elmsln.png") {
+        for (var i = 0; i < cardimgs.length; i++) {
+            cardimgs[i].src = "https://github.com/elmsln.png";
+        }
+    }
+    else {
+        for (var i = 0; i < cardimgs.length; i++) {
+            cardimgs[i].src = "https://i.pinimg.com/originals/fb/30/d0/fb30d0ef2fd6304ebcb837a59afb8817.jpg";
+        }
+    }
 });
 
 //change bg color
@@ -24,14 +46,12 @@ document.querySelector('#changebg').addEventListener('click', function (e) {
     var cards = document.getElementsByClassName('base');
     if (cards[0].classList.contains('change-color')) {
         for (var i = 0; i < cards.length; i++) {
-            var base = cards[i];
-            base.classList.remove('change-color');
+            cards[i].classList.remove('change-color');
         }
     }
     else {
         for (var i = 0; i < cards.length; i++) {
-            var base = cards[i];
-            base.classList.add('change-color')
+            cards[i].classList.add('change-color')
         }
     }
 });
